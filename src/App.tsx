dd78f6b7761a3290c1718c2084dff488065c3099
vovakiriuhins <vova11999977@gmail.com>
2023-03-29 02:45:24 +0300

    Counter app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.module.css';
+import {Counter} from "./components/Counter";
+
 
 function App() {
+
+    const [counter, setCounter] = useState<number>(0)
+
+    const incrHandler = () => {
+        setCounter(counter + 1)
+    }
+
+    const descrHandler = () => {
+        setCounter(0)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Counter
+         counter={counter}
+         incrHandler={incrHandler}
+         descrHandler={descrHandler}
+
+     />
     </div>
   );
 }
